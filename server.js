064df64a0e0af68dd0718cd67311387b95860ba4
Rommel Santos <rommelrenne@gmail.com>
2020-04-04 14:08:57 -0300

    Dia 5 - Construção do banco de dados com Sqlite3

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const server = express()
 
+//exportando o bando de dados
+const db = require("./db")
+
 //configuração do nunjucks
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
@@ -9,64 +12,79 @@ nunjucks.configure("views", {
     noCache: true
 })
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Curso de programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-        url: "https://github.com/rommel-s"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercício",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-        url: "https://github.com/rommel-s"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-        url: "https://github.com/rommel-s"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaoke",
-        category: "Diversão",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-        url: "https://github.com/rommel-s"
-    }
-]
-
 //configuração de arquivos estáticos
 server.use(express.static("public"))
+
+//habilitar req.body
+server.use(express.urlencoded({ extended:true }))
 
 //criei uma rota / e capturo o pedido do cliente para responder
 server.get("/", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
-
-    let lastIdeas = []
-    for ( idea of reversedIdeas ) {
-        if (lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no Banco de dados")
         }
-    }
+        
+        const reversedIdeas = [...rows].reverse()
 
-    return res.render("index.html", { ideas: lastIdeas })
+        let lastIdeas = []
+        for ( idea of reversedIdeas ) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+    
+        return res.render("index.html", { ideas: lastIdeas })
+    })
 
 })
 
 server.get("/ideas", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
-    return res.render("ideas.html", { ideas: reversedIdeas})
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no Banco de dados")
+        }
 
+        const reversedIdeas = [...rows].reverse()
+        return res.render("ideas.html", { ideas: reversedIdeas})
+
+    })
+
+
+})
+
+server.post("/", function (req, res) {
+    //inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?, ?, ?, ?, ?);
+    `
+    
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no Banco de dados")
+        }
+
+        return res.redirect("/ideas")
+    })
 })
 
 //liguei o servidor na porta 3000
